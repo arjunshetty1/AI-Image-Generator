21121133cce2974767301138938ba74e77e20134
arjunshetty1 <arjunjayapura@mail.com>
2023-11-08 23:44:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { NextUIProvider } from "@nextui-org/react";
+const App = () => {
+  return (
+    <NextUIProvider>
+      <div className="bg-slate-600">App</div>
+    </NextUIProvider>
+  );
+};
+
+export default App;
